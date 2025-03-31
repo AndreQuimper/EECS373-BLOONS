30657716d0c866e8a315521c8f8fe537b7ba953b
Cadin Cross <rocket48875@gmail.com>
2025-03-31 18:22:12 -0400

    send coords is multi threaded

--- uart_raspberrypi/send_coords.py
+++ uart_raspberrypi/send_coords.py
@@ -2,7 +2,11 @@
 import time
 import serial
 import random
+import threading
 
+mutex = threading.Lock()
+cnt = 0
+coords = []
 
 ser = serial.Serial(
         port='/dev/ttyAMA0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
@@ -13,17 +17,37 @@ ser = serial.Serial(
         timeout=None
 )
 
-while 1:
+def display():
+    global cnt 
+    global coords
+    while 1:
+        mutex.acquire()
+        size = cnt
+        my_coords = coords 
+        mutex.release()
+
         x=ser.readline()
         print(x)
-        time.sleep(0.01)
-        # Generate two random integer coordinates
-        size = random.randint(1, 5)
-        x, y = random.randint(0, 255), random.randint(0, 255)
-        data = bytearray([x, y])
-        for i in range(size-1):
-                x, y = random.randint(0, 255), random.randint(0, 255)
-                data += bytearray([x, y])
+        data = bytearray(my_coords)
         ser.write(bytes([size]))
-        time.sleep(0.10)
         ser.write(data)
+
+def gen():
+    global cnt 
+    global coords
+    while 1:
+        my_cnt = random.randint(1, 10)
+        my_coords = []
+        for i in range(my_cnt):
+            my_coords.append(random.randint(0, 255))
+        mutex.acquire()
+        cnt = my_cnt
+        coords = my_coords
+        mutex.release()
+        time.sleep(random.randint(1, 8))
+
+t1 = threading.Thread(target=display, args=())
+t2 = threading.Thread(target=gen, args=())
+t2.start()
+t1.start()
+t2.join()
